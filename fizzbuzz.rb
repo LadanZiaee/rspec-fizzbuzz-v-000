9e7bac0e65f4f19b49008424ae91a3dd6d94d87b
Giancarlo Zabaleta <gzabaleta48@gmail.com>
2019-06-24 18:42:55 +0000

    Automatically backed up by Learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -1,13 +1,11 @@
 # Don't forget! This file needs to be 'required' in its spec file
 # See README.md for instructions on how to do this
-def fizzbuzz(int)
-  if int % 3 == 0 and int % 5 == 0
-    return "FizzBuzz"
-  elsif int % 5 == 0
-    return "Buzz"
-  elsif int % 3 == 0
-    return  "Fizz"
-
-   end
-
- end
+def fizzbuzz(fizz)
+  if fizz % 3 == 0 
+    "Fizz"
+    elsif fizz % 5 == 0 
+      "Buzz"
+      elsif fizz % 3 == 0 && fizz % 5 == 0
+        "FizzBuzz"
+  end
+end
