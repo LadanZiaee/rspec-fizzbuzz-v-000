cc37cad547faf2209446389e4c6ab098317aedf2
Giancarlo Zabaleta <gzabaleta48@gmail.com>
2019-06-24 18:44:02 +0000

    Automatically backed up by Learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -1,11 +1,11 @@
 # Don't forget! This file needs to be 'required' in its spec file
 # See README.md for instructions on how to do this
 def fizzbuzz(fizz)
-  if fizz % 3 == 0 
-    "Fizz"
+  if fizz % 3 == 0 && fizz % 5 == 0
+    "FizzBuzz"
     elsif fizz % 5 == 0 
       "Buzz"
-      elsif fizz % 3 == 0 && fizz % 5 == 0
-        "FizzBuzz"
+      elsif fizz % 3 == 0 
+        "Fizz"
   end
 end
